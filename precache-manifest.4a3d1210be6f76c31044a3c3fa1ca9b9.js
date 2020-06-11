0aacb915df54d27f3913c8a30bf3d4684f58bbf1
suman <samp.suman@gmail.com>
2020-06-11 11:26:29 +0530

    Updates

---
+++ precache-manifest.4a3d1210be6f76c31044a3c3fa1ca9b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81dcc72b7708d4b008c258bddf486dc8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "78f2cd2a28a68bd0e144",
+    "url": "/static/css/main.d1ae8061.chunk.css"
+  },
+  {
+    "revision": "f75708c7d02414a2707f",
+    "url": "/static/js/2.b57afa73.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.b57afa73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78f2cd2a28a68bd0e144",
+    "url": "/static/js/main.e5fb1ebb.chunk.js"
+  },
+  {
+    "revision": "b4ba650b238755144e2a",
+    "url": "/static/js/runtime-main.e5fc83a6.js"
+  }
+]);
